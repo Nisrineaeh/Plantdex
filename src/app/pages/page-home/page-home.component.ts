394e3ae15e11e40e717962c885ddfae5da8017b7
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-07-11 16:46:44 +0200

    http

--- src/app/pages/page-home/page-home.component.ts
+++ src/app/pages/page-home/page-home.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PlantService } from 'src/app/services/plant.service';
 
 @Component({
   selector: 'app-page-home',
   templateUrl: './page-home.component.html',
   styleUrls: ['./page-home.component.css']
 })
-export class PageHomeComponent {
+export class PageHomeComponent implements OnInit {
 
+    constructor(private plantService : PlantService){}
+
+    ngOnInit(){
+      this.plantService.getPlants().subscribe((data)=>{
+        console.log(data);
+      });
+    }
 }
