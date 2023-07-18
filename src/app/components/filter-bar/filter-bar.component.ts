b38b50f2b27fa8b49a6de0864239f719dd2dc69b
Nisrineaeh <nisrineaitelhaj@yahoo.fr>
2023-07-18 12:19:56 +0200

    constitution tableau de categorie coché

--- src/app/components/filter-bar/filter-bar.component.ts
+++ src/app/components/filter-bar/filter-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 
 @Component({
@@ -8,11 +8,28 @@ import { Component, Input } from '@angular/core';
 })
 export class FilterBarComponent {
   @Input() plantsCategorie!: string[];
+  categoriesChecked : string[]=[];
+  // @Output() clickItem = new EventEmitter<string>();
 
      onCheckCategory(e: Event) {
      console.log(e);     
      const target = e.target as HTMLInputElement;
      console.log("valeur de la checkbox: ", target.value);
      console.log("est t'elle coché ?: ", target.checked);
-   }
+
+     if(target.checked){
+      if(this.categoriesChecked.length === this.plantsCategorie.length){
+        this.categoriesChecked = [];
+      }
+      this.categoriesChecked.push(target.value);
+     }else {
+      this.categoriesChecked = this.categoriesChecked.filter((categ)=> categ !== target.value);
+     }
+     if(this.categoriesChecked.length === 0){
+      this.categoriesChecked = [...this.plantsCategorie];
+     }
+     console.log(this.categoriesChecked);
+     // .emit() de notre output devra ce faire a la fin de cet methode
+     
+    }
 }
